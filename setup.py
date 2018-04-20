f4c60a4f791d571e4f887160b885342f793d72cc
Beta Faccion <betafcc@gmail.com>
2018-04-20 13:21:40 -0300

    Bumps version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='pll',
-      version='0.1.1',
+      version='0.1.2',
       author='Beta Faccion',
       author_email='betafcc@gmail.com',
       packages=['pll'],
